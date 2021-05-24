091459ad665807742078115b26bb6b010824accb
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-05-23 23:14:36 -0500

    using useRef hook 🎣
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const  storedUserLoggedIn = localStorage.getItem('isLoggedIn');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  useEffect(()=>{
+    if(storedUserLoggedIn === '1'){
+      setIsLoggedIn(true); 
+    }
+  }, [])
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn'); 
     setIsLoggedIn(false);
   };
 
@@ -26,6 +33,6 @@ function App() {
       </main>
     </React.Fragment>
   );
-}
+} 
 
 export default App;
